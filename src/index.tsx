ced67c1beccabe54e7c341888a6fe8b6b1d2f069
Maxim Mineev <mms1401@ya.ru>
2024-02-20 00:15:16 +0500

    react init, add eslint

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './index.css';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+
+const rootNode = document.getElementById('root');
+const root = createRoot(rootNode as HTMLElement);
+root.render(<App />);
