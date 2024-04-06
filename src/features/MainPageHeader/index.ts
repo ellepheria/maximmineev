2236a764789d046084844e59c61ad1b32ecf9d7b
Maxim Mineev <mms1401@ya.ru>
2024-04-06 23:55:27 +0500

    main page header component

---
+++ src/features/MainPageHeader/index.ts
@@ -0,0 +1,1 @@
+export { MainPageHeader } from './ui/MainPageHeader';
--- src/pages/MainPage/ui/MainPage/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage/MainPage.tsx
@@ -1,20 +1,12 @@
 import { memo } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
-import MainImage from 'shared/assets/images/main-photo.jpg';
+import { Page } from 'widgets/Page/Page';
+import { MainPageHeader } from 'features/MainPageHeader';
 import cls from './MainPage.module.scss';
-import { Page } from '../../../../widgets/Page/Page';
-import { HStack, VStack } from '../../../../shared/ui/Stack';
-import { Text, TextGap } from '../../../../shared/ui/Text/Text';
 
 interface MainPageProps {
     className?: string;
 }
-
-const description: string[] = [
-    // eslint-disable-next-line max-len
-    'Я занимаюсь фронтенд-разработкой с 2020 года, сейчас учусь на втором курсе Уральского Федерального Университета на специальности "Программная инженерия".',
-    'Есть опыт разработки на Vue.js, в основном пишу на React, TypeScript',
-];
 
 const MainPage = (props: MainPageProps) => {
     const {
@@ -23,22 +15,7 @@ const MainPage = (props: MainPageProps) => {
 
     return (
         <Page className={classNames(cls.MainPage, {}, [className || ''])}>
-            <VStack max gap="32">
-                <HStack max gap="32" justify="between">
-                    <VStack
-                        className={cls.description}
-                        gap="16"
-                    >
-                        <Text
-                            title="Привет! Меня зовут Максим"
-                        />
-                        {description.map((block) => (
-                            <Text text={block} key={block} />
-                        ))}
-                    </VStack>
-                    <img src={MainImage} alt="main" className={cls.photo} />
-                </HStack>
-            </VStack>
+            <MainPageHeader />
         </Page>
     );
 };
