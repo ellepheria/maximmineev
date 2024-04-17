03cc7f7411d5927a7eeefc0ffdf97ab7377bfc94
Maxim Mineev <mms1401@ya.ru>
2024-04-17 21:23:15 +0500

    fix posts page skeletons

--- src/pages/PostsPage/ui/PostsPageSkeletons/PostsPageSkeletons.tsx
+++ src/pages/PostsPage/ui/PostsPageSkeletons/PostsPageSkeletons.tsx
@@ -1,15 +1,12 @@
 import { memo } from 'react';
 import { VStack } from 'shared/ui/Stack';
 import { Skeleton } from 'shared/ui/Skeleton/Skeleton';
-import { Page } from 'widgets/Page/Page';
 
 export const PostsPageSkeletons = memo(() => (
-    <Page>
-        <VStack max gap="32">
-            <Skeleton width="100%" height="900px" />
-            <Skeleton width="100%" height="900px" />
-            <Skeleton width="100%" height="900px" />
-            <Skeleton width="100%" height="900px" />
-        </VStack>
-    </Page>
+    <VStack max gap="32">
+        <Skeleton width="100%" height="900px" />
+        <Skeleton width="100%" height="900px" />
+        <Skeleton width="100%" height="900px" />
+        <Skeleton width="100%" height="900px" />
+    </VStack>
 ));
