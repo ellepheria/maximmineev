9765286369cd2aa5fd9a46c599b6920d47e481ba
Maxim Mineev <mms1401@ya.ru>
2024-04-22 14:08:40 +0500

    post details page skeletons

--- src/entities/Post/ui/PostDetailsSkeletons/PostDetailsSkeletons.tsx
+++ src/entities/Post/ui/PostDetailsSkeletons/PostDetailsSkeletons.tsx
@@ -1,19 +1,20 @@
 import { memo } from 'react';
 import { Skeleton } from 'shared/ui/Skeleton/Skeleton';
 import { HStack } from 'shared/ui/Stack';
+import { Grid } from 'shared/ui/Grid/Grid';
+import cls from './PostDetailsSkeletons.module.scss';
 
 export const PostDetailsSkeletons = memo(() => (
-    <>
+    <Grid className={cls.grid}>
         <HStack justify="center" max>
-            <Skeleton width="100%" height="525px" />
+            <Skeleton width="100%" className={cls.image} />
         </HStack>
         <HStack gap="16" justify="between" max>
-            <Skeleton width="500px" height="50px" />
-            <Skeleton width="100px" height="40px" />
+            <Skeleton width="100%" height="100px" />
         </HStack>
         <Skeleton width="100%" height="200px" />
         <Skeleton width="100%" height="200px" />
         <Skeleton width="100%" height="200px" />
         <Skeleton width="100%" height="200px" />
-    </>
+    </Grid>
 ));
