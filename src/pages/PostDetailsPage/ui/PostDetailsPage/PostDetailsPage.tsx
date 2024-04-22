42d8b53478a6a78077b60b70178babe17bcc2ad9
Maxim Mineev <mms1401@ya.ru>
2024-04-22 13:19:15 +0500

    post details page adaptive all versions

--- src/pages/PostDetailsPage/ui/PostDetailsPage/PostDetailsPage.tsx
+++ src/pages/PostDetailsPage/ui/PostDetailsPage/PostDetailsPage.tsx
@@ -2,13 +2,17 @@ import { memo } from 'react';
 import { useParams } from 'react-router-dom';
 import { Page } from 'widgets/Page/Page';
 import { PostDetails } from 'entities/Post/ui/PostDetails/PostDetails';
+import { Grid } from 'shared/ui/Grid/Grid';
+import cls from './PostDetailsPage.module.scss';
 
 const PostDetailsPage = () => {
     const { id } = useParams<{ id: string }>();
 
     return (
         <Page>
-            <PostDetails id={id} />
+            <Grid className={cls.grid}>
+                <PostDetails id={id} />
+            </Grid>
         </Page>
     );
 };
